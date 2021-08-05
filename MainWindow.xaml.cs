ca68120bc1f096f1053c559f2e1677d640e9702b
Daniel Ward <danielaj_28@hotmail.co.uk>
2021-08-06 00:28:41 +0100

    Closes #6 Copy from source to destinations

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wrangler
 {
@@ -31,12 +19,15 @@ namespace Wrangler
 
 			UpdateDriveList();
 
-			presets.Add(new Preset { name = "ACamOliver" });
-			presets.Add(new Preset { name = "BCamMalcom" });
-			presets.Add(new Preset { name = "CCamDan" });
-			presets.Add(new Preset { name = "DCamGopro" });
-			presets.Add(new Preset { name = "ECamGoPro" });
-			presets.Add(new Preset { name = "FCamSpare" });
+			var p = new Preset
+			{
+				name = "Test"
+			};
+
+			p.paths.Add(@"G:\wrangler\a");
+			p.paths.Add(@"G:\wrangler\b");
+			presets.Add(p);
+
 			cbxPreset.ItemsSource = presets;
 		}
 
@@ -47,7 +38,8 @@ namespace Wrangler
 
 			foreach (DriveInfo drive in drives)
 			{
-				if (!drive.DriveType.Equals(DriveType.Network)){
+				if (!drive.DriveType.Equals(DriveType.Network)&& !drive.DriveType.Equals(DriveType.Fixed))
+				{
 					devices.Add(new Device { 
 						driveLetter = drive.Name, 
 						name = drive.VolumeLabel,
@@ -62,6 +54,54 @@ namespace Wrangler
 		private void btnDeviceRefresh_Click(object sender, RoutedEventArgs e)
 		{
 			UpdateDriveList();
+		}
+
+		private void btnStart_Click(object sender, RoutedEventArgs e)
+		{
+			Device sourceDevice = (Device)cbxSources.SelectedItem;
+
+			if (sourceDevice==null)
+			{
+				MessageBox.Show("Need to select a removable drive to copy from.");
+				return;
+			}
+
+			Preset targetPreset = (Preset)cbxPreset.SelectedItem;
+
+			if (targetPreset == null || targetPreset.paths.Count==0)
+			{
+				MessageBox.Show("Need to select a preset to copy too.");
+				return;
+			}
+
+			List<string> sourceFilePaths = new List<string>();
+
+			sourceFilePaths.AddRange(GetAllFiles(sourceDevice.driveLetter));
+
+
+			//Copy
+			foreach (var destinationPath in targetPreset.paths)
+			{
+				foreach (string sourceFilePath in sourceFilePaths)
+				{
+					string relativeFilePath = sourceFilePath.Substring(sourceFilePath.IndexOf("\\",0),sourceFilePath.Length- sourceFilePath.IndexOf("\\", 0));
+					string middlePath = relativeFilePath.Substring(0, relativeFilePath.LastIndexOf("\\"));
+					string directoryPathToCreate = destinationPath+middlePath;
+					string filePathDestination = destinationPath+ relativeFilePath;
+
+					Directory.CreateDirectory(directoryPathToCreate);
+					File.Copy(sourceFilePath, filePathDestination);
+				}
+			}
+
+			//Verify
+
+
+		}
+
+		private IEnumerable<string> GetAllFiles(string dir)
+		{
+			return Directory.GetFiles(dir, "*", SearchOption.AllDirectories);
 		}
 	}
 }
